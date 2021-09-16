28a11f2b307d9be0a1eeac1da0803bdf1e65f37b
Pitisha96 <petrikin1996@gmail.com>
2021-09-17 00:06:55 +0300

    init application

--- src/main/java/by/itransition/itrwebapp/controllers/MainPageController.java
+++ src/main/java/by/itransition/itrwebapp/controllers/MainPageController.java
@@ -1,12 +1,22 @@
 package by.itransition.itrwebapp.controllers;
 
+import by.itransition.itrwebapp.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainPageController {
+    private final UserService userService;
+
+    @Autowired
+    public MainPageController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/")
     public String greeting(){
+
         return "mainpage";
     }
 }
---
+++ src/main/java/by/itransition/itrwebapp/models/User.java
@@ -0,0 +1,22 @@
+package by.itransition.itrwebapp.models;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String username;
+    private String social;
+    private Date firstDate;
+    private Date lastDate;
+    private boolean blocked;
+}
