eaa67f694038b98222b67df8214c010f3643e1c4
Pitisha96 <petrikin1996@gmail.com>
2021-09-16 21:01:25 +0300

    init application

---
+++ src/main/java/by/itransition/itrwebapp/ItrwebappApplication.java
@@ -0,0 +1,13 @@
+package by.itransition.itrwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItrwebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItrwebappApplication.class, args);
+    }
+
+}
